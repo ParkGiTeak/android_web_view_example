cf20285c4f245b6ba786dd3ebb0b3319f3b61838
ParkGiTeak <rl980901@naver.com>
2024-05-31 00:26:15 +0900

    MainActivity WebView 표출 및 Url전달 처리 완료. WebViewFragment WebView 기본 설정 및 웹사이트 표출.

--- app/src/main/java/com/example/androidwebviewexample/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/androidwebviewexample/ui/activity/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.androidwebviewexample.ui.activity
 
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import android.window.OnBackInvokedCallback
 import androidx.activity.OnBackPressedCallback
 import com.example.androidwebviewexample.R
 import com.example.androidwebviewexample.databinding.ActivityMainBinding
+import com.example.androidwebviewexample.ui.fragment.WebViewFragment
 import kotlin.properties.Delegates
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var backPressedTime: Long = 0L
+    private var mWebViewFragment: WebViewFragment? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,23 +27,55 @@ class MainActivity : AppCompatActivity() {
 
         onBackPressedDispatcher.addCallback(this@MainActivity, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                if(System.currentTimeMillis() > backPressedTime + 2000) {
-                    backPressedTime = System.currentTimeMillis()
-                    Toast.makeText(this@MainActivity, "한 번 더 누르면 종료", Toast.LENGTH_SHORT).show()
-                } else if(System.currentTimeMillis() <= backPressedTime + 2000) {
-                    exitProcess(0)
+                if(mWebViewFragment != null) {
+                    mWebViewFragment?.webViewExit()
+                    mWebViewFragment = null
+                } else {
+                    if(System.currentTimeMillis() > backPressedTime + 2000) {
+                        backPressedTime = System.currentTimeMillis()
+                        Toast.makeText(this@MainActivity, "한 번 더 누르면 종료", Toast.LENGTH_SHORT).show()
+                    } else if(System.currentTimeMillis() <= backPressedTime + 2000) {
+                        exitProcess(0)
+                    }
                 }
             }
         })
-
         initWidget()
     }
 
     private fun initWidget() {
         if(::binding.isInitialized) {
             binding.btnShowWebView.setOnClickListener {
-                // WebViewFragment 표출
+                hideKeyBord()
+                val url = getUrl()
+                if(url != null) {
+                    if(mWebViewFragment == null) {
+                        mWebViewFragment = WebViewFragment()
+                    }
+                    mWebViewFragment?.let { webViewFragment ->
+                        val bundle = Bundle()
+                        bundle.putString("webUrl", url)
+                        webViewFragment.arguments = bundle
+                        this@MainActivity.supportFragmentManager.beginTransaction()
+                            .add(R.id.layout_web_view_fragment_container, webViewFragment, "WebViewFragment")
+                            .commitAllowingStateLoss()
+                    } ?: Log.e("webViewApp", "mWebViewFragment is null")
+                } else {
+                    Toast.makeText(this@MainActivity, "URL을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
             }
         }
+    }
+
+    private fun getUrl(): String? {
+        val urlInputViewText = binding.etUrlInputView.text.toString()
+        return urlInputViewText.ifEmpty {
+            null
+        }
+    }
+
+    private fun hideKeyBord() {
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.etUrlInputView.windowToken, 0)
     }
 }
--- app/src/main/java/com/example/androidwebviewexample/ui/fragment/WebViewFragment.kt
+++ app/src/main/java/com/example/androidwebviewexample/ui/fragment/WebViewFragment.kt
@@ -1,27 +1,125 @@
 package com.example.androidwebviewexample.ui.fragment
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Message
+import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.androidwebviewexample.databinding.FragmentWebViewBinding
+import com.example.androidwebviewexample.ui.activity.MainActivity
 
 class WebViewFragment : Fragment() {
     private var _binding: FragmentWebViewBinding? = null
     private val binding get() = _binding!!
 
+    private var mUrl: String? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentWebViewBinding.inflate(inflater, container, false)
+        mUrl = arguments?.getString("webUrl")
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initWidget()
+    }
+
+    private fun initWidget() {
+        binding.btnWebViewExit.setOnClickListener {
+            webViewExit()
+        }
+        binding.webViewMainContent.apply {
+            mUrl?.let {
+                setWebViewSettings(this.settings)
+                this.webViewClient = WebViewFragmentWebViewClient()
+                this.webChromeClient = WebViewFragmentWebChromeClient()
+                this.loadUrl(it)
+            } ?: kotlin.run {
+                Log.e("webViewApp", "mUrl is null")
+                webViewExit()
+            }
+            this.setOnKeyListener { _, keyCode, event ->
+                if(keyCode == KeyEvent.KEYCODE_BACK
+                    && event.action == MotionEvent.ACTION_DOWN) {
+                    if(this.canGoBack()) {
+                        this.goBack()
+                        true
+                    } else {
+                        false
+                    }
+                } else {
+                    false
+                }
+            }
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setWebViewSettings(settings: WebSettings) {
+        settings.apply {
+            this.javaScriptEnabled = true // JavaScript 사용 여부
+            this.javaScriptCanOpenWindowsAutomatically = true // JavaScript가 창을 자동으로 열 수 있도록 있는 여부
+            this.loadsImagesAutomatically = true // 이미지 자동 로드
+            this.domStorageEnabled = true // 로컬 저장소를 이용하여 dom 허용
+            this.useWideViewPort = true // wide viewport 사용 유무
+            this.loadWithOverviewMode = true // WebView 내 컨텐츠가 WebView보다 크면 스크린사이즈에 맞추는 설정
+            this.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK // WebView Cache 설정
+            this.builtInZoomControls = false // 내장 Zoom 컨트롤 사용 유무
+            this.setSupportZoom(false) // WebView Zoom 사용 여부
+            this.setSupportMultipleWindows(true) // WebView내 새창 뛰우기 사용 유무
+        }
+    }
+
+    fun webViewExit() {
+        if(activity is MainActivity) {
+            (activity as MainActivity).supportFragmentManager.apply {
+                this.beginTransaction()
+                    .remove(this@WebViewFragment)
+                    .commitAllowingStateLoss()
+                this.popBackStack()
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    inner class WebViewFragmentWebViewClient: WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            // todo url load url을 보고 상황에 따른 처리
+            return super.shouldOverrideUrlLoading(view, request)
+        }
+    }
+
+    inner class WebViewFragmentWebChromeClient: WebChromeClient() {
+        override fun onCreateWindow(view: WebView?, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?): Boolean {
+            // todo 팝업 및 새창 처리
+            return super.onCreateWindow(view, isDialog, isUserGesture, resultMsg)
+        }
+
+        override fun onCloseWindow(window: WebView?) {
+            super.onCloseWindow(window)
+            // todo webView 쿠키 혹은 history Clear
+        }
+
+        /* 웹사이트 플레이어 기본 포스터 제거 */
+        override fun getDefaultVideoPoster(): Bitmap? {
+            return Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888)
+        }
     }
 }
